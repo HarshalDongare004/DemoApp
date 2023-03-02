69ecf609274c03bad8c6b10cb9788d2152af4a51
HarshalDongare004 <harshaldongare004@gmail.com>
2023-03-02 23:42:05 +0530

    first commit with eclipse

---
+++ DemoApp/src/DemoApp.java
@@ -0,0 +1,15 @@
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {10,20,30,40,50};
+		for(int ele:a) {
+			System.out.println(ele);
+		}
+		String name = "sachin";
+		int count = name.toUpperCase().length();
+		System.out.println("The lenght of string is "+ count);
+	}
+
+}
